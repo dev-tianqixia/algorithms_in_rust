70affa8ae0470eb5dff2be0cce9fa99685c3badc
dev-tianqixia <txiadev@gmail.com>
2020-02-25 00:12:14 +0800

    heap.swim

--- src/heap/binary_heap.rs
+++ src/heap/binary_heap.rs
@@ -1,3 +1,6 @@
+// a complete binary tree represented in level order using array(vector)
+// parent of node k: (k-1)/2
+// children of node k: 2*k+1, 2*k+2
 struct Heap<T: PartialOrd> {
     v: Vec<T>
 }
@@ -6,6 +9,19 @@ impl<T: PartialOrd> Heap<T> {
     fn new(v: Vec<T>) -> Heap<T> {
         Heap {
             v: v
+        }
+    }
+}
+
+// heap operations normally make a simple modification that violates the heap invariant,
+// then traveling through and modify the heap as required to regain the heap invariant.
+impl<T: PartialOrd> Heap<T> {
+    // swim implements bottom-up reheapify
+    fn swim(&mut self, index: usize) {
+        let mut i = index;
+        while i > 0 && self.v[i] > self.v[(i-1)/2] {
+            self.v.swap(i, (i-1)/2);
+            i = (i-1)/2;
         }
     }
 }
